66a747a4bc854fc5786e4423eb3dfc69916f710e
Aryan Vishwakarma <aryan.vishwakarma0902@gmail.com>
2023-07-07 14:49:59 +0530

    adminRoute are updated to get feedbacks

--- controller/adminRoute.js
+++ controller/adminRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { updateInsights } = require("../insights.js");
 const Insights = require("../model/insightSchema");
+const async = require("hbs/lib/async.js");
+const { Feedback } = require("../model/feedbackSchema.js");
 
 router.patch("/update-insights", (req, res) => {
   updateInsights();
@@ -18,5 +20,35 @@ router.get("/insights", async (req, res) => {
     res.status(500).json({ error: "Error getting insights" });
   }
 });
+
+router.get("/feedback/all", async (req, res) => {
+  try {
+    const feedbacks = await Feedback.find()
+    res.status(200).json(feedbacks)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Error getting feedbacks" })
+  }
+})
+
+router.get("/feedback/", async (req, res) => {
+  try {
+    const productid = req.query.productid
+    const productname = req.query.productname
+    if (productid) {
+      const feedbacks = await Feedback.find({ productid })
+      res.status(200).json(feedbacks)
+    } else if (productname) {
+      const feedbacks = await Feedback.find({ productname })
+      res.status(200).json(feedbacks)
+    } else {
+      res.status(400).json({ error: "Invalid query" })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Error getting feedbacks" })
+  }
+})
+
 
 module.exports = router;
